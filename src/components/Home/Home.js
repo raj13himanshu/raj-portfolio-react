a41f22857bb297bd59469de2cd303e5a03bb7868
Himanshu Raj <63577940+raj13himanshu@users.noreply.github.com>
2021-12-14 21:34:44 +0530

    Update Home.js

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -21,7 +21,7 @@ function Home() {
               </h1>
 
               <h1 className="heading-name">
-                I'M
+                I
                 <strong className="main-name"> Himanshu Raj </strong>
               </h1>
 
